4691baf551ebf3c73e315044f369ad022b636019
Richard Li <RichardLi@gmail.com>
2018-12-04 20:48:13 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+    <head>
+<title>php1</title>
+    </head>
+
+<body>
+    <?php require_once('nav.php');?>
+     <?php
+            global $n, $z;
+            $n = "jim";
+            $f = "smith";
+            $x = 3;
+            $t = false;
+            $a = array('val1', 'val2', "jim");
+            $a2 = ['apple', 'grapes','bananas'];
+            $a3 = [
+                "fn"=>"mark",
+                "ln"=>"garcia",
+                "city"=>"ny",
+                "age"=> 20
+            ];
+            
+            function sumof(){
+                global $n, $z;
+                if( $n == "james" ){
+                    $z = "james is in the house";
+                } else {
+                    $z = "james is home sick";
+                };
+            };
+        
+            sumof();
+        
+        ?>
+        <h1>hello, <?php echo $n.' '.$f; ?></h1>
+        <h1><?php echo 'my name is '. $n.' '.$f; ?></h1>
+        <h1><?php print_r($a2); ?></h1>
+        <h1><?php echo $z ?></h1>
+        <h1>
+            <?php
+                foreach( $a3 as $index => $value ){
+                    echo $index.'<br>';
+                };
+            
+            ?>
+        </h1>
+
+    </body>
+</html>
---
+++ portfolio.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+    
+    </title>
+    </head>
+    
+    <body>
+    <?php require_once('nav.php');?>
+        
+    <h1>portfolio</h1>
+    </body>
+</html>
